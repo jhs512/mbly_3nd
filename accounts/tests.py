d419a2f6d1bdff16228c82076f5b6a48bf9d2ca9
jhs512 <jangka512@gmail.com>
2022-01-24 16:29:45 +0900

    작업

--- accounts/tests.py
+++ accounts/tests.py
@@ -6,5 +6,5 @@ from accounts.models import User
 
 class AccountsTests(TestCase):
     def test_create_user(self):
-        user: User = User.objects.create_super(username='admin', password='admin', name='홍길동', email='test@test.com')
+        user: User = User.objects.create_user(username='admin', password='admin', name='홍길동', email='test@test.com')
         self.assertTrue(user.username == 'admin')
